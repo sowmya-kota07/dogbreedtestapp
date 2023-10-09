d86db9722f7c2827c6dda690e242a67f16f46130
Abhishek Raj <abhi7cronaldo@gmail.com>
2023-10-09 13:58:38 -0700

    feat: add cms stack

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,29 @@
 import { Backend } from '@aws-amplify/backend';
+import { AmplifyCustomData } from './data/resource.js';
 import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
 
-new Backend({
-  auth,
-  data,
-});
+try {
+  const backend = new Backend({
+    auth
+  });
+
+  const stack = backend.getOrCreateStack('AmplifyCMS');
+  const backendId = stack.node.getContext('backend-id');
+  const branchName = stack.node.tryGetContext('branch-name') || 'sandbox';
+
+  const authResources = backend.resources.auth.resources;
+  const { authenticatedUserIamRole, unauthenticatedUserIamRole, cfnResources, userPool } = authResources;
+
+  new AmplifyCustomData(stack, 'AmplifyCustomData', {
+    authRole: authenticatedUserIamRole!,
+    unauthRole: unauthenticatedUserIamRole!,
+    identityPoolId: cfnResources.identityPool.
+      logicalId, userPool,
+    backendId,
+    branchName
+  });
+} catch (e) {
+  console.log(e);
+}
+
+
--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,10 @@
-import { defineData } from '@aws-amplify/backend-graphql';
+// import { defineData } from '@aws-amplify/backend-graphql';
+import { AmplifyGraphqlApi } from '@aws-amplify/graphql-api-construct'
+import { Stack } from 'aws-cdk-lib';
+import { IUserPool } from 'aws-cdk-lib/aws-cognito';
+import { AccountPrincipal, Role, Policy, PolicyStatement, Effect, IRole } from 'aws-cdk-lib/aws-iam';
+import { Bucket, HttpMethods } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
 
 const schema = `
   type Todo @model @auth(rules: [{ allow: private }]) {
@@ -7,4 +13,65 @@ const schema = `
   }
 `;
 
-export const data = defineData({ schema });
+interface Props {
+  authRole: IRole;
+  unauthRole: IRole;
+  identityPoolId: string;
+  userPool: IUserPool;
+  backendId: string;
+  branchName: string;
+}
+
+export class AmplifyCustomData extends Construct {
+  constructor(scope: Construct, name: string, props: Props) {
+    const { authRole, unauthRole, identityPoolId, userPool, backendId, branchName } = props;
+    super(scope, name);
+
+    const account = Stack.of(scope).account;
+    const region = Stack.of(scope).region;
+    const principal = new AccountPrincipal(account);
+
+    const roleName = `amplify-cms-manage-role-${backendId}-${branchName}`;
+    const cmsManageRole = new Role(scope, 'AmplifyCMSManageRole', {
+      roleName,
+      assumedBy: principal,
+    });
+    const inlineApiPolicy = new Policy(scope, 'AmplifyCMSManageRolePolicy');
+    inlineApiPolicy.addStatements(new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: [`arn:aws:appsync:${region}:*:apis/*`],
+      actions: ['appsync:GraphQL']
+    }));
+    cmsManageRole.attachInlinePolicy(inlineApiPolicy);
+    const roleArn = `arn:aws:iam::${account}:role/${roleName}`;
+
+    const api = new AmplifyGraphqlApi(scope, 'AmplifyGraphqlApi', {
+      definition: {
+        schema,
+        functionSlots: [],
+      },
+      authorizationModes: {
+        adminRoles: [Role.fromRoleArn(scope, 'AmplifyCMSRole', roleArn)],
+        defaultAuthorizationMode: 'AWS_IAM',
+        iamConfig: {
+          authenticatedUserRole: authRole,
+          unauthenticatedUserRole: unauthRole,
+          identityPoolId,
+        },
+        userPoolConfig: {
+          userPool
+        }
+      }
+    })
+    const codegenAssetsBucket = api.node.findChild('AmplifyCodegenAssets').node.findChild('AmplifyCodegenAssetsBucket') as Bucket;
+    codegenAssetsBucket.addCorsRule({
+      allowedMethods: [HttpMethods.GET],
+      allowedHeaders: ['*'],
+      allowedOrigins: ['https://localhost.console.aws.amazon.com:3000',
+        'https://*.console.aws.amazon.com/amplify/home']
+    });
+
+  }
+}
+
+// export const data = defineData({ schema });
